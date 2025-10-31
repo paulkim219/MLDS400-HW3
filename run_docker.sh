3479a21947a7a5a1cee006efcedfeb5e972801ac
paulkim219 <paulkim219@gmail.com>
2025-10-30 20:03:46 -0500

    Adding DockerFile, requirements, python file, and docker command bash script
    These four files are being added as a way to create a Docker image for a python script with requirements. The bash script is there for running the Docker commands with ease

---
+++ run_docker.sh
@@ -0,0 +1,1 @@
+docker build . --tag "pythonapp" --file ./scripts/DockerFile
---
+++ src/scripts/app.py
@@ -0,0 +1,6 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+
+train_df = pd.read_csv('data/test.csv')
+train_df.head()
